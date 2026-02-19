9181aee1e6b97dcc5173c60245aa03657d6f2def
rado31 <dova.jarvis@gmail.com>
2026-02-19 15:27:04 +0100

    refactor: read from env variables for config

--- crates/core/src/config.rs
+++ crates/core/src/config.rs
@@ -1,7 +1,7 @@
-pub const API_BASE_URL: &str = "";
-pub const API_KEY: &str = "";
-pub const UPDATE_URL: &str = "http://localhost:8080/info";
-pub const DEVICE_ID: &str = "";
-pub const CLIENT_ID: &str = "REDACTED_CLIENT_ID";
+pub const API_BASE_URL: &str = env!("KIOSK_API_URL");
+pub const API_KEY: &str = env!("KIOSK_API_KEY");
+pub const UPDATE_URL: &str = env!("KIOSK_UPDATE_URL");
+pub const DEVICE_ID: &str = env!("KIOSK_DEVICE_ID");
+pub const CLIENT_ID: &str = env!("KIOSK_CLIENT_ID");
 pub const PUBLIC_KEY_PATH: &str = "keys/public.key";
 pub const POPULAR_STATION_IDS: [u32; 5] = [17, 27, 32, 49, 4];
